0da6c62b7a490490a1c41a33a7ff971fae1afce2
Laura Berge <lauraeberge@gmail.com>
2019-11-16 12:23:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, occasion) {
+    let messages = []
+    for (let i = 0; i < nameArray.length; i++) {
+        messages.push(`Thank you, ${nameArray[i]}, for the wonderful ${occasion} gift!`);
+    }
+
+    return messages
+}
+
+function countDown(n) {
+    while (n > -1) {
+        console.log(n)
+        n--
+    }
+}
